a568bb62f902268fc920019525d7c032da127b48
Garrett Griffey <garrettwgriffey@gmail.com>
2020-03-15 20:24:18 -0500

    properly linked JS

---
+++ assets/script.js
@@ -0,0 +1,37 @@
+
+// API key
+var APIKey = "50fb093a801ea7cec3ad6773d6bd6102";
+// var cityName = "";
+// URL to query the database
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=Nashville&appid=" + APIKey;
+
+// console.log(queryURL)
+
+
+// AJAX call
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function (response) {
+  $('#searchBtn').click(function () {
+    console.log(queryURL);
+    console.log(response);
+
+
+    $(".city").html("<h1>" + response.name + "Weather Details</h1>");
+    $(".wind").text("Wind Speed: " + response.wind.speed);
+    $(".humidity").text("Humidity: " + response.main.humidity);
+    $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+    console.log("Wind Speed: " + response.wind.speed);
+    console.log("Humidity: " + response.main.humidity);
+    console.log("Temperature (F): " + tempF);
+
+
+
+  });
+
+
+
+})
