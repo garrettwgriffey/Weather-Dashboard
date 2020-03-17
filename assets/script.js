6028af66502e2d3ae2c09895816eec8f69f8f7d1
Garrett Griffey <garrettwgriffey@gmail.com>
2020-03-16 20:33:12 -0500

    working on dynamic city search

--- assets/script.js
+++ assets/script.js
@@ -3,61 +3,68 @@
 var APIKey = "50fb093a801ea7cec3ad6773d6bd6102";
 // var cityName = "#userInput";
 // URL to query the database
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-  "q=Nashville&appid=" + APIKey;
-
-
+// URL for UV index
 var uVQueryURL = "https://api.openweathermap.org/data/2.5/uvi?f&lat=36.17&lon=-86.78&appid=" + APIKey;
+
+// queryURL for city
+// var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + queryTerm + "&appid=" + APIKey;
+// console.log(queryURL);
+
+var queryTerm = " ";
 
 // console.log(queryURL)
 // var showTime = $("#currentDay").text(setTime);
 // console.log(showTime);
 
-// function setTime() {
-//   var time = moment().format("MMMM Do YYYY, h:mm a");
-//   return time;
-// }
-// console.log(setTime());
-
-  // AJAX call for QueryURL
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).then(function (response) {
-    $('#searchBtn').on('click', function (e) {
-      e.preventDefault();
-      // id of userInput
-     
+function searchCity(queryURL) {
+  var queryTerm = $("#input")
+    .val()
+    .trim();
+// queryURL for city search
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + queryTerm + "&appid=" + APIKey;
       console.log(queryURL);
+
+// AJAX call for QueryURL
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function (response) {
+  $('#searchBtn').on('click', function (e) {
+    e.preventDefault();
+    
+    // console.log(queryURL);
+    // console.log(response);
+  
+
+
+    $(".city").html("<h1>" + response.name + "Weather Details</h1>");
+    $(".date").text(moment().format("MMM Do YYYY"));
+    $(".wind").text("Wind Speed: " + response.wind.speed + "mph");
+    $(".humidity").text("Humidity: " + response.main.humidity);
+
+    // Convert temp to Fahrenheit
+    var tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+    // add temp to html
+    $(".temp").text("Temperature (K) " + response.main.temp);
+    $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
+
+
+    console.log("Wind Speed: " + response.wind.speed);
+    console.log("Humidity: " + response.main.humidity);
+    // console.log("Temperature (F): " + tempF);
+
+    $.ajax({
+      url: uVQueryURL,
+      method: "GET"
+    }).then(function (response) {
       console.log(response);
+      $(".UV").text("UV Index: " + response.value);
+
+    })
+
+  });
 
 
-      $(".city").html("<h1>" + response.name + "Weather Details</h1>");
-      $(".date").text(moment().format("MMM Do YYYY"));
-      $(".wind").text("Wind Speed: " + response.wind.speed);
-      $(".humidity").text("Humidity: " + response.main.humidity);
-
-      // Convert temp to Fahrenheit
-      var tempF = (response.main.temp - 273.15) * 1.80 + 32;
-
-      // add temp to html
-      $(".temp").text("Temperature (K) " + response.main.temp);
-      $(".tempF").text("Temperature (F) " + tempF.toFixed(2));
-
-    
-      console.log("Wind Speed: " + response.wind.speed);
-      console.log("Humidity: " + response.main.humidity);
-      // console.log("Temperature (F): " + tempF);
-
-      $.ajax({
-        url: uVQueryURL,
-        method: "GET"
-      }).then(function(response) {
-        console.log(response);
-        $(".UV").text("UV Index: " + response.value);
-    
-      })
-
-
-    });
-  })
+})
+}
